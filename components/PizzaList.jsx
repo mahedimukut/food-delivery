992232d96169d3e9e6bb2aea1979b00d7ef6cc20
mahedimukut <mokot222@gmail.com>
2023-05-10 01:12:16 +0600

    first commit of food delivery

---
+++ components/PizzaList.jsx
@@ -0,0 +1,19 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+const PizzaList = ({ pizzaList }) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim debitis nisi sequi porro atque consectetur temporibus possimus neque ab reprehenderit magni quia.</p>
+            <div className={styles.wrapper}>
+
+                {pizzaList.map((pizza) => (
+                    <PizzaCard key={pizza._id} pizza={pizza} />
+                ))}
+
+            </div>
+        </div>
+    );
+}
+
+export default PizzaList;
